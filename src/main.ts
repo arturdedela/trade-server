855eb2a2056959622065cf81681c14b35cfd67cd
Artur <arturdedela@gmail.com>
2020-01-08 22:14:22 +0300

    Add triggers

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Connection } from 'typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,6 +10,49 @@ async function bootstrap() {
       credentials: true,
     },
   });
+
+  await app.get(Connection).query(`
+    CREATE TABLE logs (
+        date timestamp default now() NOT NULL,
+        info varchar NOT NULL
+    );
+
+    DROP TRIGGER IF EXISTS on_add_user on user_entity;
+    DROP FUNCTION IF EXISTS log_user();
+
+    CREATE FUNCTION log_user() RETURNS TRIGGER
+        LANGUAGE plpgsql
+    AS $$
+    BEGIN
+        INSERT INTO logs (date, info) VALUES (DEFAULT, CONCAT('Created user ', new.email));
+        RETURN new;
+    END
+    $$;
+
+    CREATE TRIGGER on_add_user
+        AFTER INSERT ON user_entity
+        FOR EACH ROW
+    EXECUTE PROCEDURE log_user();
+
+
+
+    DROP TRIGGER IF EXISTS on_add_security on security_entity;
+    DROP FUNCTION IF EXISTS log_security();
+
+    CREATE FUNCTION log_security() RETURNS TRIGGER
+        LANGUAGE plpgsql
+    AS $$
+    BEGIN
+        INSERT INTO logs (date, info) VALUES (DEFAULT, CONCAT('Created security ', new."fullName"));
+        RETURN new;
+    END
+    $$;
+
+    CREATE TRIGGER on_add_security
+        AFTER INSERT ON security_entity
+        FOR EACH ROW
+    EXECUTE PROCEDURE log_security();
+  `);
 
   app.useGlobalPipes(new ValidationPipe());
 
